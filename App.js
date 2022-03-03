390aa7c4f33f74eed80f3d8c827bb42ecb304c0d
Mostafa <moayedpour.m@gmail.com>
2022-03-03 14:40:26 +0330

    💄⚰️  add new code and style, remove some code

--- App.js
+++ App.js
@@ -51,6 +51,17 @@ function Section({children, title}) {
   )
 }
 
+function Section2(props) {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>just bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    </View>
+  )
+}
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark'
 
@@ -64,25 +75,16 @@ function App() {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+            <Section2></Section2>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          {/* <LearnMoreLinks /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -90,6 +92,9 @@ function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -105,6 +110,14 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 })
 
